f112ee5f957522ef1b1484fbd64129ee701ef80e
3Dever96 <134976722+3Dever96@users.noreply.github.com>
2026-09-21 13:53:02 -0400

    PlayerHUD implemented

---
+++ Assets/Scripts/UI/PlayerHealthbar.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthbar : MonoBehaviour
+{
+    [SerializeField] private Slider healthbar;
+    private CharacterStats player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterStats>();
+
+        player.OnHit.AddListener(UpdateHealthbar);
+        player.OnDeath.AddListener(UpdateHealthbar);
+    }
+
+    public void UpdateHealthbar()
+    {
+        healthbar.value = Mathf.FloorToInt((player.currentHp / player.stats["HP"].Value) * 100f);
+    }
+
+    private void OnEnable()
+    {
+        if (player != null)
+        {
+            player.OnHit.AddListener(UpdateHealthbar);
+            player.OnDeath.AddListener(UpdateHealthbar);
+        }
+    }
+
+    private void OnDisable()
+    {
+        player.OnHit.RemoveListener(UpdateHealthbar);
+        player.OnDeath.RemoveListener(UpdateHealthbar);
+    }
+}
